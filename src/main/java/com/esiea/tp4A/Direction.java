a2a160860c5378ff5496e8c11c4586929235c28f
Souly <jonch93@gmail.com>
2020-02-26 18:57:41 +0100

    Initialisation du projet + mise en place du mouvement

---
+++ src/main/java/com/esiea/tp4A/Direction.java
@@ -0,0 +1,11 @@
+package com.esiea.tp4A.domain;
+
+public enum Direction {
+	NORTH, EAST, SOUTH, WEST;
+	
+	/*public Direction left() {
+		int leftOrdinal = ordinal() - 1;
+		Direction[] values = Direction.values();
+		return values[leftOrdinal < 0 ? leftOrdinal = values.length - 1 : leftOrdinal];
+	}*/
+}
---
+++ src/main/java/com/esiea/tp4A/MarsRoverImpl.java
@@ -0,0 +1,88 @@
+package com.esiea.tp4A.domain;
+
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
+
+public class MarsRoverImpl implements MarsRover {
+	
+	private Position position;
+		
+	public MarsRover initialize(Position position) {
+        setPosition(position);
+		return this;
+    }
+
+    public MarsRover updateMap(PlanetMap map) {
+        return this;
+    }
+
+    public MarsRover configureLaserRange(int range) {
+        return this;
+    }
+
+    public Position move(String command) {
+    	if(command.isEmpty()) {
+    		return Position.of(0, 0, Direction.NORTH);
+    	}else {
+    		CharacterIterator ci = new StringCharacterIterator(command);
+    		
+    		while(ci.current() != CharacterIterator.DONE) {
+    			switch(ci.current()) {
+    				case 'f' :
+    					if(position.getDirection() == Direction.NORTH) {
+    						setPosition(Position.of(position.getX(), position.getY() + 1, position.getDirection()));
+    					}else if(position.getDirection() == Direction.EAST) {
+    						setPosition(Position.of(position.getX() + 1, position.getY(), position.getDirection()));
+    					}else if(position.getDirection() == Direction.SOUTH) {
+    						setPosition(Position.of(position.getX(), position.getY() - 1, position.getDirection()));
+    					}else if(position.getDirection() == Direction.WEST) {
+    						setPosition(Position.of(position.getX() - 1, position.getY(), position.getDirection()));
+    					}
+    					break;
+    				case 'b' :
+    					if(position.getDirection() == Direction.NORTH) {
+    						setPosition(Position.of(position.getX(), position.getY() - 1, position.getDirection()));
+    					}else if(position.getDirection() == Direction.EAST) {
+    						setPosition(Position.of(position.getX() - 1, position.getY(), position.getDirection()));
+    					}else if(position.getDirection() == Direction.SOUTH) {
+    						setPosition(Position.of(position.getX(), position.getY() + 1, position.getDirection()));
+    					}else if(position.getDirection() == Direction.WEST) {
+    						setPosition(Position.of(position.getX() + 1, position.getY(), position.getDirection()));
+    					}
+    					break;
+    				case 'r' :
+    					if(position.getDirection() == Direction.NORTH) {
+    						setPosition(Position.of(position.getX(), position.getY(), Direction.EAST));
+    					}else if(position.getDirection() == Direction.EAST) {
+    						setPosition(Position.of(position.getX(), position.getY(), Direction.SOUTH));
+    					}else if(position.getDirection() == Direction.SOUTH) {
+    						setPosition(Position.of(position.getX(), position.getY(), Direction.WEST));
+    					}else if(position.getDirection() == Direction.WEST) {
+    						setPosition(Position.of(position.getX(), position.getY(), Direction.NORTH));
+    					}
+    					break;
+    				case 'l' :
+    					if(position.getDirection() == Direction.NORTH) {
+    						setPosition(Position.of(position.getX(), position.getY(), Direction.WEST));
+    					}else if(position.getDirection() == Direction.EAST) {
+    						setPosition(Position.of(position.getX(), position.getY(), Direction.NORTH));
+    					}else if(position.getDirection() == Direction.SOUTH) {
+    						setPosition(Position.of(position.getX(), position.getY(), Direction.EAST));
+    					}else if(position.getDirection() == Direction.WEST) {
+    						setPosition(Position.of(position.getX(), position.getY(), Direction.SOUTH));
+    					}
+    					break;
+    				default :
+    					break;
+    			}
+    				
+    		}
+    	}
+        return Position.of(position.getX(), position.getY(), position.getDirection());
+    }
+    
+    public void setPosition(Position posi) {
+    	this.position = posi;
+    }
+
+}
