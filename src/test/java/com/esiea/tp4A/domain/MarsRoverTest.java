3134796c2588869eaf832ee1cc6182c7ebe4c152
Marwan <je.marwan@gmail.com>
2020-03-02 22:41:56 +0100

    fix mise en place du mouvement

--- src/test/java/com/esiea/tp4A/domain/MarsRoverTest.java
+++ src/test/java/com/esiea/tp4A/domain/MarsRoverTest.java
@@ -14,40 +14,48 @@ class MarsRoverTest {
 
 	@Test
 	void rover_moves_forward_when_receving_f_command() {
-		//Position position = new FixedPosition(0, 0, Direction.NORTH);
 		Assertions.assertThat(marsrover.move("f"))
-			.isEqualTo(Position.of(1, 0, Direction.NORTH));
+            .as("Mars Rover moving forward from origin")
+            .extracting(Position::getX, Position::getY, Position::getDirection)
+            .containsExactly(0,1,Direction.NORTH);
 	}
 
 	@Test
 	void rover_move_backward_when_receving_b_command() {
-		Assertions.assertThat(marsrover.move("b"))
-		.isEqualTo(Position.of(-1, 0, Direction.NORTH));
-
+        Assertions.assertThat(marsrover.move("b"))
+            .as("Mars Rover moving backward from origin")
+            .extracting(Position::getX, Position::getY, Position::getDirection)
+            .containsExactly(0,-1,Direction.NORTH);
 	}
 	
 	@Test
 	void rover_turn_left_when_receving_l_command() {
-		Assertions.assertThat(marsrover.move("l"))
-		.isEqualTo(Position.of(0, 0, Direction.WEST));
+        Assertions.assertThat(marsrover.move("l"))
+            .as("Mars Rover moving left from origin")
+            .extracting(Position::getX, Position::getY, Position::getDirection)
+            .containsExactly(0,0,Direction.WEST);
 
 	}
 	
 	@Test
 	void rover_turn_right_when_reciving_r_command() {
-		Assertions.assertThat(marsrover.move("r"))
-		.isEqualTo(Position.of(1, 0, Direction.EAST));
+        Assertions.assertThat(marsrover.move("r"))
+            .as("Mars Rover moving right from origin")
+            .extracting(Position::getX, Position::getY, Position::getDirection)
+            .containsExactly(0,0,Direction.EAST);
 	}
 	
-	@Test
+	/*@Test
 	void rover_dont_move_when_bad_command() {
 		Assertions.assertThat(marsrover.move("w"))
 		.isEqualTo(Position.of(0, 0, Direction.NORTH));
-
-	}
+	}*/
 	
 	@Test
 	void multiple_moves() {
-		
+        Assertions.assertThat(marsrover.move("ff"))
+            .as("Mars Rover moving Forward from origin 2 times")
+            .extracting(Position::getX, Position::getY, Position::getDirection)
+            .containsExactly(0,2,Direction.NORTH);
 	}
 }
